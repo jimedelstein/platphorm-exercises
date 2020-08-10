be3b79b870369ebebef8d309d06e4ff32d843e7f
John Paxton <pax@speedingplanet.com>
2020-08-09 20:31:39 -0400

    Config, setup, appearance

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main className="container-fluid">
+        <nav className="navbar navbar-expand navbar-dark bg-dark mb-3">
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li>
+                <NavLink
+                  activeClassName="active"
+                  to="/exercise-01"
+                  className="nav-link"
+                  exact
+                >
+                  Exercise 1
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  activeClassName="active"
+                  to="/exercise-02"
+                  className="nav-link"
+                >
+                  Exercise 2
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  activeClassName="active"
+                  to="/exercise-03"
+                  className="nav-link"
+                >
+                  Exercise 3
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  activeClassName="active"
+                  to="/exercise-04"
+                  className="nav-link"
+                >
+                  Exercise 4
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="row pl-1">
+          <div className="col">
+            <Switch>
+              <Route path="/users">
+                <UsersManager />
+              </Route>
+              <Route path="/transactions">
+                <TransactionsManager />
+              </Route>
+              <Route path="/demos">
+                <DemosManager />
+              </Route>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </main>
+    </Router>
   );
 }
 
